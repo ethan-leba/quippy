3817eb77793fd796bb7ef0e0b60d0324ff129366
ethan-leba <ethanleba5@gmail.com>
2020-12-21 11:09:53 -0500

    Add IO wrapper for event processing to websocket ns

--- src/clj/quippy/routes/websockets.clj
+++ src/clj/quippy/routes/websockets.clj
@@ -1,9 +1,12 @@
 (ns quippy.routes.websockets
   (:require
    [clojure.tools.logging :as log]
+   [quippy.game.core :as game]
    [immutant.web.async :as async]))
 
 (defonce channels (atom {}))
+
+(defonce game-state (atom game/default-game-state))
 
 (defn connect! "Establishes a connection"
   [channel type]
@@ -14,11 +17,13 @@
   (log/info "close code:" code "reason:" reason)
   (swap! channels #(dissoc channel %)))
 
-(defn process-user-event! [channel msg]
-  (doseq [channel (keys @channels)]
-    (async/send! channel msg)))
-
-(defn handle-action! [channel msg])
+(defn process-user-event!
+  "Handles the IO of event processing"
+  [channel msg]
+  (let [[new-state responses] (game/process-user-event @game-state msg)]
+    (compare-and-set! game-state new-state)
+    (doseq [[channel response] responses]
+      (async/send! channel response))))
 
 (defn websocket-callbacks
   "WebSocket callback functions"
